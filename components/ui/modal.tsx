5258e79ea5ba7af2619ff373ba740844cafda475
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2023-12-27 02:52:52 +0600

    "cart-store-hook"

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Dialog, Transition } from '@headlessui/react'
 import { X } from 'lucide-react';
 import { Fragment } from 'react';
@@ -38,7 +37,7 @@ const Modal: React.FC<ModalProps> = ({
                 <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
                   <div className="absolute right-4 top-4">
                     <IconButton onClick={onClose} icon={<X size={15} />} />
-                    
+
                   </div>
                   {children}
                 </div> 
